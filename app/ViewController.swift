646368519a9c30af021f6e03b36a4ccf67cb0515
tokorom <tokorom@gmail.com>
2021-06-22 15:51:59 +0900

    Sync AVPlayer

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -6,18 +6,20 @@
 //
 
 import AVKit
-import Combine
 import GroupActivities
 import UIKit
 
 class ViewController: AVPlayerViewController {
     static let movieURL: URL? = URL(string: "https://devstreaming-cdn.apple.com/videos/wwdc/2019/408bmshwds7eoqow1ud/408/hls_vod_mvp.m3u8")
 
+    private var groupSession: GroupSession<MovieWatchingActivity>?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupPlayer()
         prepareSharePlay()
+        listenForGroupSession()
     }
 
     private func setupPlayer() {
@@ -31,29 +33,6 @@ class ViewController: AVPlayerViewController {
     }
     
     private func prepareSharePlay() {
-        CoordinationManager.shared.prepareToPlay()
-    }
-}
-
-struct MovieWatchingActivity: GroupActivity {
-    static let activityIdentifier = "work.spinners.SharePlaySample.GroupWatching"
-    
-    var metadata: GroupActivityMetadata {
-        var metadata = GroupActivityMetadata()
-        metadata.fallbackURL = ViewController.movieURL
-        metadata.previewImage = UIImage(named: "wwdc19")?.cgImage
-        metadata.title = "Sample"
-        metadata.subtitle = "WWDC19 Session Video"
-        return metadata
-    }
-}
-
-final class CoordinationManager {
-    static let shared = CoordinationManager()
-    
-    private var subscriptions = Set<AnyCancellable>()
-    
-    func prepareToPlay() {
         let activity = MovieWatchingActivity()
         
         async {
@@ -67,5 +46,28 @@ final class CoordinationManager {
             default: ()
             }
         }
+    }
+
+    private func listenForGroupSession() {
+        async {
+            for await session in MovieWatchingActivity.sessions() {
+                groupSession = session
+                player?.playbackCoordinator.coordinateWithSession(session)
+                session.join()
+            }
+        }
+    }
+}
+
+struct MovieWatchingActivity: GroupActivity {
+    static let activityIdentifier = "work.spinners.SharePlaySample.GroupWatching"
+    
+    var metadata: GroupActivityMetadata {
+        var metadata = GroupActivityMetadata()
+        metadata.fallbackURL = ViewController.movieURL
+        metadata.previewImage = UIImage(named: "wwdc19")?.cgImage
+        metadata.title = "Sample"
+        metadata.subtitle = "WWDC19 Session Video"
+        return metadata
     }
 }

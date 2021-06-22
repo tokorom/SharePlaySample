4fb2cd6ee3981c3574a7cf81eb20d783d786041d
tokorom <tokorom@gmail.com>
2021-06-22 15:25:15 +0900

    Support Group Activities

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import AVKit
+import Combine
+import GroupActivities
 import UIKit
 
 class ViewController: AVPlayerViewController {
@@ -15,6 +17,7 @@ class ViewController: AVPlayerViewController {
         super.viewDidLoad()
 
         setupPlayer()
+        prepareSharePlay()
     }
 
     private func setupPlayer() {
@@ -25,5 +28,44 @@ class ViewController: AVPlayerViewController {
         let player = AVPlayer(url: movieURL)
         self.player = player
         player.play()
+    }
+    
+    private func prepareSharePlay() {
+        CoordinationManager.shared.prepareToPlay()
+    }
+}
+
+struct MovieWatchingActivity: GroupActivity {
+    static let activityIdentifier = "work.spinners.SharePlaySample.GroupWatching"
+    
+    var metadata: GroupActivityMetadata {
+        var metadata = GroupActivityMetadata()
+        metadata.fallbackURL = ViewController.movieURL
+        metadata.previewImage = UIImage(named: "wwdc19")?.cgImage
+        metadata.title = "Sample"
+        metadata.subtitle = "WWDC19 Session Video"
+        return metadata
+    }
+}
+
+final class CoordinationManager {
+    static let shared = CoordinationManager()
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    func prepareToPlay() {
+        let activity = MovieWatchingActivity()
+        
+        async {
+            switch await activity.prepareForActivation() {
+            case .activationDisabled:
+                break
+            case .activationPreferred:
+                activity.activate()
+            case .cancelled:
+                break
+            default: ()
+            }
+        }
     }
 }
